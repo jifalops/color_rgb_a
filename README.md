1c6c7a57743af408394e577998dadb601aeea9f7
Jacob Phillips <jifalops@gmail.com>
2018-08-25 16:58:01 -0400

    First commit, imported from external project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# color_rgb_a
+
+Represents a color in the red-green-blue scheme with an optional alpha component. Designed for use with both Flutter and CSS.
---
+++ test/color_rgb_a_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+import 'package:color_rgb_a/color_rgb_a.dart';
+
+void main() {
+  final color = Color(0xFF123456);
+  test('No platform dependencies', () {});
+}
